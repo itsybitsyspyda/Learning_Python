1084ab605b2b1262ecd4d9154dba984fe6175050
itsybitsyspyda <kimathicodes@gmail.com>
2025-11-03 17:15:02 +0300

    Basic Input/Output

--- lesson2.py
+++ lesson2.py
@@ -1,3 +1,9 @@
 #Making The Program Interactive
 
 #Get user input
+
+name = input("What is your name?")
+age = int(input("How old are you?"))
+
+#Printing the results
+print("Hello" , name, "you are" , age, "years old")
