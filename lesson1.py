999aadfa36c5134d781d76a75ed51df49222a95c
itsybitsyspyda <kimathicodes@gmail.com>
2025-11-03 16:37:42 +0300

    Lesson One Python

---
+++ lesson1.py
@@ -0,0 +1,32 @@
+#integers
+myint = 7
+print(myint)
+
+#float
+myfloat = 7.0
+print(myfloat)
+
+#string
+mystring = "hello"
+print(mystring)
+
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "Hello"
+world = "World"
+helloworld = hello + " " + world
+print(helloworld)
+
+a, b = 3, 4
+print(a, b)
+
+#Error Will Not Work
+one = 1
+two = 2
+hello = "Hello"
+print(one + two + hello)
+
